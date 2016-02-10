979ff359b78d51765819a7149a128bcee321ab4c
anstrickland <anstrickland@email.meredith.edu>
2016-02-10 16:47:14 -0500

    Update LoginCheck.php

--- LoginCheck.php
+++ LoginCheck.php
@@ -17,19 +17,19 @@
    
    date_default_timezone_set("America/New_York");
    
-$em = @$_POST[email4log]; // raw email address, may have bad stuff
-$emas = addslashes( $em ); // slashes version 
-$pw = @$_POST[password4log];
-$pwas = addSlashes( $pw );
+$em = @$_POST[email]; // raw email address, may have bad stuff
+$email = addslashes( $em ); // slashes version 
+$password = @$_POST[password];
+$pwas = addSlashes( $password );
 
-if($em!="" && $em==$emas && $pw!="" && $pw==$pwas ) // no funny stuff
+if($em!="" && $em==$email && $password!="" && $password==$pwas ) // no funny stuff
 {
    // the user is trying to log in, so check db for name and password.
    // We used to check for confirmed=0, because registrants and users were
    // in the same table, but now we have a separate table.  Customer table
    // is now ONLY confirmed.
    $query="SELECT * FROM User WHERE email='$em'"
-       ." AND password='$pw'"
+       ." AND password='$password'"
        ." ;";
    // echo "query=".$query."<br />";
    $result=mysql_query($query);
@@ -57,8 +57,8 @@ if($em!="" && $em==$emas && $pw!="" && $pw==$pwas ) // no funny stuff
       $_SESSION['first']=$row['first'];
       $_SESSION['userID']=$row['userID'];
        
-      if (!$bug) { header("Location: Begin.php"); exit; }
-      else { echo "bug pause, click <a href=\"RHC.php\">here</a> to continue. <br />\n";}
+      if (!$bug) { header('Location: http://mcbitlab.com/thebookclub/begin.html'); exit; }
+      else { echo "bug pause, click <a href=\"home.html\">here</a> to continue. <br />\n";}
    }
    else
    {
@@ -82,7 +82,7 @@ else // there was funny typing, don't even check database, just reject
    // echo "sessionID=".session_id();
    if ($bug && $stat="login successful" )
    {
-      echo "<a href=\"RHC.php\"> success page </a> ";
+      echo "<a href=\"home.html\"> success page </a> ";
    }
    echo "<br /> Just backup or go to the home page if you want to try again.";
 ?>
