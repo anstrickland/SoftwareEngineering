a5d63de06a8f7743f36009f13525eb380f311b5d
anstrickland <anstrickland@email.meredith.edu>
2016-02-17 16:32:06 -0500

    Update SellProc.php

--- SellProc.php
+++ SellProc.php
@@ -1,21 +1,20 @@
 <?php
-
-	include("includeMe.php");
+  include("includeMe.php");
   include("openDB.php");
   openDB();
 
-	$productID = addslashes($_POST['productID']);
+	$counter = addslashes($_POST['productID']);
 	$title = addslashes($_POST['title']);
 	$author = addslashes($_POST['author']);
 	$edition = addslashes($_POST['edition']);
 	$ISBN = addslashes($_POST['ISBN']);
    
-$query="insert into Product(productID, title, author, edition, ISBN) values('$productID','$title', '$author', '$edition', '$ISBN')";
+$query="insert into Product(productID, title, author, edition, ISBN) values('$counter','$title', '$author', '$edition', '$ISBN')";
 
 
 
-   $result=mysql_query($query);
-   if ($result==0) { noerror( $result ); }
+  //$result=mysql_query($query);
+  //if ($result==0) { noerror( $result ); }
    
- header("Location: Sale.php");	exit;
+ header("Location: http://mcbitlab.com/thebookclub/sellMatch.php");	exit;
 ?>
