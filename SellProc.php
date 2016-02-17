529ef6d1a3f1143c75f49f880f40cd5753d1b23a
anstrickland <anstrickland@email.meredith.edu>
2016-02-17 17:40:25 -0500

    Update SellProc.php

--- SellProc.php
+++ SellProc.php
@@ -3,18 +3,21 @@
   include("openDB.php");
   openDB();
 
-	$counter = addslashes($_POST['productID']);
 	$title = addslashes($_POST['title']);
 	$author = addslashes($_POST['author']);
 	$edition = addslashes($_POST['edition']);
 	$ISBN = addslashes($_POST['ISBN']);
+	
+	$query = "SELECT MAX(productID) from Product";
+        $row = mysql_fetch_row($result);
+        $productID = $row[0] + 1;
    
-$query="insert into Product(productID, title, author, edition, ISBN) values('$counter','$title', '$author', '$edition', '$ISBN')";
+$query="insert into Product(productID, title, author, edition, ISBN) values('$productID','$title', '$author', '$edition', '$ISBN')";
 
 
 
-  //$result=mysql_query($query);
-  //if ($result==0) { noerror( $result ); }
+  $result=mysql_query($query);
+  if ($result==0) { noerror( $result ); }
    
  header("Location: http://mcbitlab.com/thebookclub/sellMatch.php");	exit;
 ?>
