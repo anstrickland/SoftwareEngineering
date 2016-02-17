872fe16dd8183a677a074d22cfabf6362cbe561c
Kristin Long <ktlong@email.meredith.edu>
2016-02-17 16:25:50 -0600

    logout page

---
+++ logout.php
@@ -0,0 +1,17 @@
+ <?php
+
+/* 
+This page logs the user out.  
+*/
+   session_start();
+   $_SESSION['loginok'] = "no";
+   ?>
+<html>
+	<head>
+	  <title>  Logout </title> 
+	</head>
+</html>
+
+<?php
+header("Location: http://mcbitlab.com/thebookclub/home.html"); 
+?>
