291e6809fc63c19e3d02d96146a0ad3bd8f62882
Kristin Long <ktlong@email.meredith.edu>
2016-02-17 16:05:40 -0600

    matching books

---
+++ BuyMatch.php
@@ -0,0 +1,27 @@
+
+
+<?php
+ include("includeMe.php");
+ include("openDB.php"); //in the same folder
+  openDB();
+?>
+
+
+<html>
+<body>
+
+<h1> List of Book Matches </h1>
+
+<?php
+	
+	$q="SELECT *from Product where ISBN='$ISBN';" ;
+ 	$result = mysql_query($q); //returning whats in the table
+	if(noerror($result))
+	{
+		tabledump($result);
+	}
+	
+	 
+?>
+</body>
+</html>
