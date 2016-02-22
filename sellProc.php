e809475f28ab9f54e3a4edb5f6c9619b39256ac5
anstrickland <anstrickland@email.meredith.edu>
2016-02-22 16:39:02 -0500

    Update sellProc.php

--- sellProc.php
+++ sellProc.php
@@ -3,22 +3,36 @@
   include("openDB.php");
   openDB();
 
+      $newId= getMaxOrderId() +1;
+
 	$title = addslashes($_POST['title']);
 	$author = addslashes($_POST['author']);
 	$edition = addslashes($_POST['edition']);
 	$ISBN = addslashes($_POST['ISBN']);
 	
-	$query = "SELECT MAX(productID) from Product";
-        $row = mysql_fetch_row($result);
-        $productID = $row[0] + 1;
    
-$query="insert into Product(productID, title, author, edition, ISBN) values('$productID','$title', '$author', '$edition', '$ISBN')";
+$query="insert into Product(productID, title, author, edition, ISBN) values('$newId','$title', '$author', '$edition', '$ISBN')";
   $result=mysql_query($query);
   if ($result==0) { noerror( $result ); }
    
-$query="insert into Sales(userID, productID, price, whenPosted) values('$userID','$productID', '$price', '$whenPosted')";
+$query="insert into Sales(userID, productID, price, whenPosted) values('$userID','$newId', '$price', '$whenPosted')";
   $result=mysql_query($query);
   if ($result==0) { noerror( $result ); }   
+   
+   
+   	function getMaxOrderId()
+  {	
+	$maxid= 0;
+	$query= "SELECT MAX(productID) from Product;";
+	$result = mysql_query($query);
+	if(noerror($result))
+	{
+		$row = mysql_fetch_row($result);
+		$maxid= $row[0];
+	}		
+	
+	return $maxid;
+  }	
    
  header("Location: http://mcbitlab.com/thebookclub/sellMatch.php");	exit;
 ?>
