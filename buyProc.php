13683ea02beaa7cb08986f05f8b6ad9d0d03d34f
Olivia Brown <brownoli@email.meredith.edu>
2016-03-02 16:11:52 -0500

    buy processing update

--- buyProc.php
+++ buyProc.php
@@ -4,20 +4,23 @@
 	include("openDB.php");
 	openDB();
 	
-	date_default_timeszone_set("American/New_York");
+	date_default_timezone_set("America/New_York");
+	
+	
+	$newId = getMaxOrderId()+1;
 	
 	$ibsn_raw = @$_POST[isbn];	//the isbn number, unsecured
 	$isbn_slash = addslashes($isbn_raw);	//safely feature for isbn field
 	
 	$now = time();		//the current time
-	$order_placed = date("m-d-y", $now);
+	$order_placed = date("Y-m-d", $now);
 	
 	//placing the isbn number in the order table
 	//also places the date in the table (not sure if works)
-	$query = "INSERT INTO Order SET". "ISBN='".addslashes($isbn_slash)."'"
-	."whenOrderPlaced = '$order_placed';";
+	$query = "INSERT INTO Order SET"
+	. "ISBN='". addslashes($isbn_slash)."' "
+	." ;";
 	$result = mysql_query($query);
-	
 	if ($result == 0) { noerror($result);}
 	
 	//need function to generate userID
@@ -34,10 +37,5 @@
 		
 		return $maxid;
 	}
-	
-?>
-	
-	
-	
-
+	 header('Location: http://mcbitlab.com/thebookclub/start.html'); exit;
 ?>
