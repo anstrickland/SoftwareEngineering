f4acaa45a81f943750a42c6189a4bf9d3c737ac0
anstrickland <anstrickland@email.meredith.edu>
2016-03-01 20:53:13 -0500

    Update sellProc.php

--- sellProc.php
+++ sellProc.php
@@ -3,7 +3,7 @@
   include("openDB.php");
   openDB();
 
-      $newId= getMaxOrderId();
+      $newId= getMaxOrderId()+1;
 
 	$title = addslashes($_POST['title']);
 	$author = addslashes($_POST['author']);
