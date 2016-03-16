7f14e8d8a5692d768ba1101a86b2d0fea2f8e47a
anstrickland <anstrickland@email.meredith.edu>
2016-03-16 13:43:23 -0400

    Update sellProc.php

--- sellProc.php
+++ sellProc.php
@@ -23,10 +23,10 @@ if ($result==0) { noerror( $result ); }
  	$price = addslashes($_POST['price']);
  	$userID="SELECT userID FROM User WHERE email = '$emailAddress'";
  	$userID2=mysql_query($userID);
- 	echo $userID2;
+ 	echo mysql_fetch_row($userID2);
  	$productID="SELECT productID FROM Product WHERE Product.ISBN= '$isbn';";
  	$productID2=mysql_query($productID);
- 	echo $productID2;
+ 	echo mysql_fetch_row($productID2);
  	$now = time();		//the current time
  	$whenPosted= date("Y-m-d", $now);
  
