7088c688d3a40f549c3e263c061f33fdde75bf52
anstrickland <anstrickland@email.meredith.edu>
2016-03-16 15:59:07 -0400

    Update buyProc.php

--- buyProc.php
+++ buyProc.php
@@ -18,8 +18,9 @@
 	//placing the isbn number in the order table
 	//also places the date in the table (not sure if works)
 	$query = "INSERT INTO Order SET"
-		." ISBN= '$isbn'" 
-		." ,whenOrderPlaced = '$orderPlaced';";
+	."        ISBN= '$isbn'" 
+	."        ,whenOrderPlaced = '$orderPlaced'"
+	.";";
 	
 	$result = mysql_query($query);
 	if ($result == 0) { noerror($result);}
