8e330be7c475a3088df5fbd8ed0c623fd0df8f5a
anstrickland <anstrickland@email.meredith.edu>
2016-03-21 15:39:00 -0400

    Update and rename begin.html to begin.php

---
+++ begin.php
@@ -0,0 +1,17 @@
+<?php  
+	include("header.php"); 
+?>
+
+<html>
+	<head>
+		<title> Libro To Go </title>
+	</head>
+	
+	<body>
+		<p>
+			Buy or Sell Your Textbooks Here.
+		</p>
+		<button type ="button"><a href="buy.php">Buy</a></button>
+		<button type ="button"><a href="sell.php">Sell</a></button>
+	</body>
+</html>
