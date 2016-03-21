f17773fe35f19df58cc990cf2c9963d981687d1a
Olivia Brown <brownoli@email.meredith.edu>
2016-03-21 16:39:49 -0400

    Added files via upload
    working buyProc.php

--- buyProc.php
+++ buyProc.php
@@ -4,42 +4,24 @@
 	include("openDB.php");
 	openDB();
 	
-	date_default_timezone_set("America/New_York");
-	
-	
 	//$newId = getMaxOrderId()+1;
 	
 	$isbn = addslashes($_POST['isbn']);	//the isbn number
-	//$isbn_slash = addslashes($isbn_raw);	//safely feature for isbn field
-	
 	$now = time();		//the current time
 	$orderPlaced = date("Y-m-d", $now);
+	$userID = ($_SESSION['userID']);
 	
 	//placing the isbn number in the order table
 	//also places the date in the table (not sure if works)
 	$query = "INSERT INTO BookOrder SET"
-	."        ISBN= '$isbn'" 
+	."		  userID = '$userID'"
+	."        ,ISBN= '$isbn'" 
 	."        ,whenOrderPlaced = '$orderPlaced'"
 	.";";
 	
 	$result = mysql_query($query);
 	if ($result == 0) { noerror($result);}
 	
-	//need function to generate userID
-
-	/*function getMaxOrderId()
-	{
-		$maxid = 0;
-		$query = "SELECT MAX(userID) from Order;";
-		$result = mysql_query($query);
-		if(noerror($result))
-		{
-			$row = mysql_fetch_row($result);
-			$maxid = $row[0];
-		}
-		
-		return $maxid;
-	}*/
 	 header('Location: http://mcbitlab.com/thebookclub/buyMatch.php'); exit;
 	
 ?>
