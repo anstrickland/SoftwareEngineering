dff7135bd910256e271a63ab5fe5bbb3c4e6bb74
SamanthaSiomkos <shsiomkos@email.meredith.edu>
2016-03-23 16:07:10 -0400

    Header File. THE REAL ONE.

---
+++ header.php
@@ -0,0 +1,53 @@
+<html lang="en">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+<head></head>
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+ <link rel="stylesheet" type="text/css" href="headerStyles.css" >
+  
+<div class="jumbotron">
+<div class="container">
+
+ <a href="logout.php" id="log"> Logout </a> 
+	
+<h1> Libro-to-Go</h1>
+<br/>
+
+<div id="menu">
+<ul class="nav nav-tabs nav-justified">
+	<li class="active"> <a href="home.html"> Home </a> </li>
+	<li> <a href="about.html"> About </a> </li>
+	<li> <a href="buy.php"> Buy </a> </li>
+	<li> <a href="sell.php"> Sell </a> </li> 
+</ul>
+</div> 
+</div> </div>
+
+<style type="text/css">
+
+h1 {
+	text-align: center;
+	}
+	
+#log {
+	color: #00bfff;
+	font-size: 15px;
+	margin-left: 1020px;
+      }	
+
+#menu a {
+		color: #00bfff;
+		font-size: 25px;		
+	}	
+	
+#menu a:hover {
+	background-color: #BEBEBE;
+	}	
+
+</style>
+
+</html>
