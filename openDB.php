494be4addb7521b10d20e89477d56ec1d017b1f7
anstrickland <anstrickland@email.meredith.edu>
2016-03-28 15:22:56 -0400

    Update openDB.php

--- openDB.php
+++ openDB.php
@@ -1,10 +1,18 @@
-<?php
-   function openDB()
-   {
-	   $host="localhost:/var/lib/mysql/mysql.sock"; // localhost doesn't work
-	   $user="samdbuser";
-	   $password="samdbpassword";
-	   mysql_connect($host,$user,$password);
-	   mysql_select_db("samdb");
-   }
+
+  // function openDB()
+   //{
+//	   $host="localhost:/var/lib/mysql/mysql.sock"; // localhost doesn't work
+//	   $user="samdbuser";
+//	   $password="samdbpassword";
+//	   mysql_connect($host,$user,$password);
+//	   mysql_select_db("samdb");
+  // }
+   
+
+<?php 
+{
+function openDB()
+$link = mysql_connect('mcbitlabcom.fatcowmysql.com', 'thebookclub', 'book2016!');  
+if (!$link) {      die('Could not connect: ' . mysql_error());  }  mysql_select_db(misdb);    
+}
 ?>
