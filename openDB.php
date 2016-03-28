676ffb47b045b6a01f6691e0280e6114b975c965
anstrickland <anstrickland@email.meredith.edu>
2016-03-28 15:25:50 -0400

    Update openDB.php

--- openDB.php
+++ openDB.php
@@ -10,8 +10,9 @@
    
 
 <?php 
-{
+
 function openDB()
+{
 $link = mysql_connect('mcbitlabcom.fatcowmysql.com', 'thebookclub', 'book2016!');  
 if (!$link) {      die('Could not connect: ' . mysql_error());  }  mysql_select_db(misdb);    
 }
