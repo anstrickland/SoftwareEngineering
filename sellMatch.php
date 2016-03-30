21a758e02ad85ed865d1525277b3c3b38c6916d8
anstrickland <anstrickland@email.meredith.edu>
2016-03-30 15:52:40 -0400

    Update sellMatch.php

--- sellMatch.php
+++ sellMatch.php
@@ -15,39 +15,7 @@
 
 	echo "Sell matches go here"
 
-/*$result=mysql_query($query);
-//if ($result==0) { noerror( $result ); }
-if (noerror($result))
-	{				
-		$nf = mysql_num_fields($result);
-		$nr = mysql_num_rows($result);
-		echo "<table border='1'> <thead>";
-		echo "<caption>";
-
-		echo "</caption> \n";
-        echo "<tr>";
-		
-			echo "<th> Name </th>";
-			echo "<th> Title </th>";
-			echo "<th> Author </th>";
-			echo "<th> Edition </th>";
-			echo "<th> ISBN </th>";
-		
-        echo "</tr>";
-        echo "</thead><tbody>";
-        
-		for($i=0; $i<$nr; $i++)
-		{
-			echo "<tr>";
-			$row=mysql_fetch_array($result);
-		
-		
-		for ( $j=0; $j<$nf; $j++ )
-		{			
-				echo "    <td>" . $row[$j] . "</td> \n";
-		}
-			echo "  </tr> \n";
-		}
-  	echo "</tbody></table>"; 
-  	*/
-  	?>
+  $result=mysql_query($query);
+  if ($result==0) { noerror( $result ); }
+  if (noerror($result))
+ ?>
