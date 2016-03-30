256a439ed2459521c2787b07320c1313ceeb7ab7
anstrickland <anstrickland@email.meredith.edu>
2016-03-30 16:37:36 -0400

    Update sellMatch.php

--- sellMatch.php
+++ sellMatch.php
@@ -15,14 +15,49 @@
   WHERE $isbn=BookOrder.ISBN";
 
 	echo "Sell matches go here";
-	echo $isbn;
+	//echo $isbn;
 
   $result=mysql_query($query);
   if ($result==0) { noerror( $result ); }
-  if (noerror($result))
+  //if (noerror($result))
   
-  $row=mysql_fetch_array($result);
-  var_dump($row);
+  //$row=mysql_fetch_array($result);
+  //var_dump($row);
+  
+  if (noerror($result))
+	{				
+		$nf = mysql_num_fields($result);
+		$nr = mysql_num_rows($result);
+		echo "<table border='1'> <thead>";
+		echo "<caption>";
+
+		echo "</caption> \n";
+        echo "<tr>";
+		
+			echo "<th> Buy ID </th>";
+			echo "<th> Name </th>";
+			echo "<th> Title </th>";
+			echo "<th> Author </th>";
+			echo "<th> Edition </th>";
+			echo "<th> ISBN </th>";
+        echo "</tr>";
+        echo "</thead><tbody>";
+		for($i=0; $i<$nr; $i++)
+		{
+			echo "<tr>";
+			$row=mysql_fetch_array($result);
+		
+		
+		for ( $j=0; $j<$nf; $j++ )
+		{			
+				echo "    <td>" . $row[$j] . "</td> \n";
+		}
+			echo "  </tr> \n";
+		}
+		   
+            echo "
+      echo "</tbody></table>";
+	}
   
   
  ?>
