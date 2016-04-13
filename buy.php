5c38892bff3bf6c30eadb22cee7491316dc766c7
SamanthaSiomkos <shsiomkos@email.meredith.edu>
2016-04-13 15:50:41 -0400

    Updated from FatCow Again

--- buy.php
+++ buy.php
@@ -7,19 +7,25 @@
 	<head>
 	<title> Libro to Go </title>
 	</head>
-	
+
+	 <center>
 	<body>
-		<header><h2>Please Enter the ISBN</h2></header>
+		<header>
+                <h2> Buy A Book </h2>
+                 <p>Please Enter the ISBN of the book you are searching for! </p>
+                </header>
 		
+
 		<section> 
 			<!-- needs the action attribute so it can send to php to search database -->
 			<form action="buyProc.php" method="POST">
-				ISBN </br>
-				<input type = "text" name = "isbn" required >
+				
+				ISBN: <input type = "text" name = "isbn" required >
 				</br>
 				</br>
 				<input type = "submit" value = "Find Book" >
 			</form>
 		</section>
+                </center>
 	</body>
 </html>
