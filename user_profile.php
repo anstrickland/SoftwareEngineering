661e4f98029e28e6dbc5e7345bf2c4652eb4be89
anstrickland <anstrickland@email.meredith.edu>
2016-04-26 10:25:27 -0400

    Update user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -3,8 +3,18 @@
   include("includeMe.php");
   include("openDB.php");
   openDB();
+  ?>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
   
-  
+<?php
   $userID=($_SESSION['userID']);
   $name=($_SESSION['name']);
   $email=($_SESSION['email']);
@@ -14,45 +24,65 @@
   echo "UserID: ".$userID."\n";
   echo "Email: ".$email."\n";
   echo "These are the books you are trying to sell: \n";
-  $q="SELECT * FROM Product 
+ 
+
+ $q="SELECT * FROM Product 
   JOIN Sales
   ON Sales.productID = Product.productID
   WHERE $userID=Sales.userID";
   
   $result=mysql_query($q);
   if ($result==0) { noerror( $result );} 
-  
+ 
   if(noerror($result))
 	{
+
+                echo "<div class=container>";
+                echo "<table class= table table-striped>";
 		$nf = mysql_num_fields($result);
 		$nr = mysql_num_rows($result);
-  
+
+
+       echo "<tr>";
+
+			echo "<th> Name </th>";
+			echo "<th> Title </th>";
+			echo "<th> Author </th>";
+			echo "<th> Edition </th>";
+		       echo "<th> Book Condition </th>";
+                      echo "<th> ISBN </th>";
+                       echo "<th> Sold? </th>";
+        echo "</tr>";
   
 	   for($i=0; $i<$nr; $i++)
 		{
 			$row =mysql_fetch_array($result);
 			
-			     $productID= $row['productID'];
-                      
-			      $title = $row['title'];
+			$productID= $row['productID'];
+                        $title = $row['title'];
 		    	$author = $row['author'];
-			$edition= $row['edition'];
+			$edition= $row['edition'];          
 		        $bookCondition= $row['bookCondition'];
-                      
-			$isbn= $row['ISBN'];
-			
-			echo "<tr> 
+                        $isbn= $row['ISBN'];
+                        $Sold= $row['Sold'];
+		
+                          echo "<tr> 
                          <td>$name</td>
-                         
                           <td>$title</td> 
                          <td>$author</td>
 			 <td>$edition</td> 
 		        <td>$bookCondition</td> 
-                         
-                        <td>$isbn</td>";
+                        <td>$isbn</td> 
+                        <td>$Sold</td>"; 
+
+                       echo "<a href='userProcess.php?Sold=$Sold&productID=$productID'> "
+		    ." update </a> <br />\n </td></tr>"; 	//set the new value of fulfilled 
 			
 		}
 				echo "</table>";
-  
+  }
   echo "</center>";
 ?>
+</div>
+</body>
+</html>
