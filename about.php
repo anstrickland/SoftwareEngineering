f3f22752bb35c7ea3f39c4520617951269754665
anstrickland <anstrickland@email.meredith.edu>
2016-04-26 09:53:30 -0400

    Update about.php

--- about.php
+++ about.php
@@ -3,12 +3,52 @@
 ?>
 
  <html>
- <head> About Me </head>
- 
+<div class="container-fluid bg-3 text-center"> 
+  <h3> <head> Welcome to Libro to Go! </head></h3>
+
  <body>
- 
+	
+	<p>This website is a culmination of the knowledge we've acquired throughout our Computer Science career. Created for our CS 407 
+	Software Engineering course, LibroToGo is a website dedicated to providing students on Meredith's campus an easy opportunity 
+	to make some money back on the plethora of textbooks collected throughout the year. 
+	Craigslist is too risky, Ebay is too expensive when PayPal, seller, and postal fees are included, and the campus bookstore is a racket 
+	- 2 bucks for a 200 dollar textbook, only to turn around and sell it for full price.
+	Facebook has somewhat mitigated the problem, but it's easy for your post to get buried during 
+	textbook buying/selling season. With LibroToGo, you can sell and buy all your textbooks in one dedicated website. 
+	Just pop in your information, browse the website for next semesters textbooks and post some books that you're finished 
+	using and we'll take care of the rest. No longer will you have to worry about receiving the correct edition or getting 
+	your textbook in time for classes. 
+
+	</br>Thanks and enjoy,
+
+	</br>The LibroToGo team
+ </p>
  </body>
- 
- 
- 
+
+</br></br></br>
+  <div class="row">
+    <div class="col-sm-3">
+     <img src="SamLinkedInPic.jpg" alt="image" class="img-circle" height="200" width="200">
+      <p>Samantha</p>
+    </div>
+    <div class="col-sm-3">
+     <img src="kristin.jpg" alt="image" class="img-circle" height="200" width="200">
+      <p>Kristin</p>
+    </div>
+    <div class="col-sm-3"> 
+     <img src="amanda.jpg" alt="image" class="img-circle" height="200" width="200">
+      <p>Amanda </p>
+    </div>
+   <div class="col-sm-3"> 
+     <img src="olivia.jpg" alt="image" class="img-circle" height="200" width="200">
+      <p>Olivia </p>   
+ </div>
+  </div>
+
+ </div>
+
+<style>
+
+</style>
+
  </html>
