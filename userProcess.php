a452e8865105f498f7d97e1d024478cf40b20226
anstrickland <anstrickland@email.meredith.edu>
2016-04-26 10:27:45 -0400

    Update userProcess.php

--- userProcess.php
+++ userProcess.php
@@ -1,1 +1,19 @@
+ <?php
 
+   include("includeMe.php");
+   include("openDB.php");
+   openDB();
+
+   
+   $productID= $_GET['productID'];		//get the order id variable
+   $Sold= $_GET['Sold']; // get the new fulfilled value
+   
+      $query = "UPDATE Product SET"
+			."       Sold='Yes' "
+			."       WHERE productID = '$productID'"
+			."       ;";
+		
+	$result=mysql_query($query);
+	
+	header('Location: http://mcbitlab.com/thebookclub/user_profile.php'); exit;   
+?>
